f91ed66c6e06e15763187bbeb52aff52dc70b926
Sebastien Ramsay <sebastien.ramsay@gmail.com>
2026-01-20 02:39:59 -0500

    move fuel pump to RCars

--- lua/entities/ramsay_gaspump/cl_init.lua
+++
@@ -1,6 +0,0 @@
-include("shared.lua")
-
--- Client-side draw function for the Entity
-function ENT:Draw()
-    self:DrawModel() -- Draws the model of the Entity. This function is called every frame.
-end
